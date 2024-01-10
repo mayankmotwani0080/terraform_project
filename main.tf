dad4d46fbf49720c594f59705be3b7dc28f63dc8
mayankmotwani0080 <142608714+mayankmotwani0080@users.noreply.github.com>
2024-01-10 14:19:01 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_ec2_host" "test" {
+  instance_type     = "t2.micro"
+  availability_zone = "us-west-2a"
+  
+  
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
